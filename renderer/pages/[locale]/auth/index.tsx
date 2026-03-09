5db79a88c3a33be52c780590b2e37bf9c8b3de78
bmplatina <kkgordon.com@gmail.com>
2026-03-09 23:56:16 +0900

    구글 로그인 요청 시 외부 링크로 동작하지 않는 문제를 해결했지만 콜백이 앱이 아닌 웹으로 리디렉션함 (API 로직 수정 필요)

--- renderer/pages/[locale]/auth/index.tsx
+++ renderer/pages/[locale]/auth/index.tsx
@@ -3,6 +3,7 @@ import { Button, Flex, Text } from "@radix-ui/themes";
 import { Card, CardTitle, CardHeader, CardContent } from "@/components/ui/card";
 import Image from "next/image";
 import LocalizedLink from "@/components/common/localized-link";
+import { openExternal } from "@/lib/utils-client";
 import { useTranslation } from "next-i18next";
 import BitmapLogo from "@/public/images/bitmaplogo-notext.png";
 import { Separator } from "@/components/ui/separator";
@@ -10,6 +11,12 @@ import LoginElements from "@/components/common/authenticate/login";
 
 export default function AccountPage() {
   const { t } = useTranslation("Authentication");
+
+  function openExternalLink(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    openExternal(event, window.electronTools);
+  }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-250px)] p-4 md:p-6 text-center">
@@ -55,7 +62,10 @@ export default function AccountPage() {
             <CardContent>
               <Flex direction="column" gap="2">
                 <Button size="3" asChild>
-                  <LocalizedLink href="https://api.prodbybitmap.com/auth/google">
+                  <LocalizedLink
+                    href="https://api.prodbybitmap.com/auth/google"
+                    onClick={openExternalLink}
+                  >
                     <Image
                       src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"
                       alt="Google"
